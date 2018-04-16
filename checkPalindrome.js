6cdbb7996d8caa67ec9ced5d5d1492c5bbe5a288
okayohyeah <ohkathy@gmail.com>
2018-04-16 16:20:03 -0700

    refactored: check for palindrome algorithm

--- checkPalindrome.js
+++ checkPalindrome.js
@@ -1,24 +1,20 @@
 // Identify if there is a palindrome
 
 function palindrome(str) {
-  //convert to lowercase
-  var lowerCase = str.toLowerCase();
-  // remove special characters
-  var noCharacters = lowerCase.replace(/[\W_]/g, "");
+  //convert to lowercase & remove special characters
+  str = str.toLowerCase().replace(/[\W_]/g, "");
   // remove whitespace and save into variable
-  var convertedString = noCharacters.replace(/\s/g,"");
+  str = str.replace(/\s/g,"");
 
-  console.log(convertedString);
+  console.log(str);
 
   // convert it to an array and reverse it
-  var array = convertedString.split("");
-  var reverseArray = array.reverse();
-  // convert it to string and save it into variable
-  var reversedString = reverseArray.join("");
+  var array = str.split("");
+  var reversedString = array.reverse().join("");
 
   console.log(reversedString);
 
-  if (convertedString === reversedString) {
+  if (str === reversedString) {
     return true;
   } else {
     return false;
